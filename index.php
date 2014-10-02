e610098a6ccb9514921127b9cf16c95592e1626e
Sven Anders Robbestad <robbestad@gmail.com>
2014-10-02 17:35:55 +0200

    Moved output below form input

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-<?php
-var_dump(file_get_contents('php://input'));
-?>
 <form action="index.php" method="post" enctype="multipart/form-data">
   <p><input type="text" name="text" value="" placeholder="Write something">
   <p><input type="file" name="file1">
   <p><button type="submit">Submit</button>
 </form>
+<?php
+var_dump(file_get_contents('php://input'));
