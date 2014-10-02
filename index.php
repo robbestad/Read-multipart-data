06df815ddb7edd8d71397b6ad8d0c6b00f008982
Sven Anders Robbestad <robbestad@gmail.com>
2014-10-02 17:34:57 +0200

    first

--- index.php
+++ index.php
@@ -6,10 +6,3 @@ var_dump(file_get_contents('php://input'));
   <p><input type="file" name="file1">
   <p><button type="submit">Submit</button>
 </form>
-<!--
-<form action="index.php" method="post"> 
-  <p><input type="text" name="text" value="" placeholder="Write something">
-  <p><input type="file" name="file1">
-  <p><button type="submit">Submit</button>
-</form>
--->
